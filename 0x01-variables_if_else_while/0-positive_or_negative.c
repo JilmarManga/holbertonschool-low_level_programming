86f630c5a68531ba73d3e6380b10c63b685b082f
JilmarManga <jilmarcode@gmail.com>
2020-09-19 17:04:45 +0000

    This code prints if the number in variable c is + or -

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main : Principal function, entry point
+ *main : Entry point
  *
- * return : Always 0
+ *return: Always 0
  */
-
 int main(void)
 {
-	int n;
+int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
