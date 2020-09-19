d60fd8e7c8a39c2419486f6023daa1f84a133668
JilmarManga <jilmarcode@gmail.com>
2020-09-19 23:28:35 +0000

    Print numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *function: Print numbers in base 10
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+char numb;
+
+for (numb = '0'; numb <= '9'; numb++)
+putchar(numb);
+putchar('\n');
+
+return (0);
+}
