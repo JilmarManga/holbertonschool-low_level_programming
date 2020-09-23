1e04f387e55c4064ac1f79770ca830760aea1ebe
JilmarManga <jilmarcode@gmail.com>
2020-09-23 04:43:02 +0000

    Function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
  * Return: 1 if greater than 0, 0 if zero, -1 if lesser than 0.
  */
 
-/*int print_sign(int n)
+int print_sign(int n)
 {
 
 	if (n > 0)
@@ -16,7 +16,7 @@
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar('-');
 		return (0);
 	}
 	else
@@ -26,21 +26,4 @@
 	}
 
 }
-*/
 
-int print_sign(int n)
-{
-	if (n > 0)
-	{
-		_putchar (43);
-		return (1);
-	}
-	if (n == 0)
-	{
-		_putchar (48);
-		return (0);
-	}
-	_putchar (45);
-	return (-1);
-
-}
