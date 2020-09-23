e94ba4bea479e88cd257e0de33bee90fe4d2da1a
JilmarManga <jilmarcode@gmail.com>
2020-09-23 03:12:00 +0000

    Code Prints if character stored in var c is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
 
 /**
- * _islower - check and print if letter stored in var c is lowercase
- *
- * @c : argument to validate
- *
- * return : 1 if c is lowercase || 0 if c isn´t lowercase
+ * _islower - checks for lowercase character.
+ * @c: character to test.
+ * Return: 1 if c is lowercase || 0 if isn´t lowercase.
  */
 
 int _islower(int c)
