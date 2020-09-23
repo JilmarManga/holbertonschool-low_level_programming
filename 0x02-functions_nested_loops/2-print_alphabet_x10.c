4d8cb78b4f10399b4cffa607c40a1618bfda5bb5
JilmarManga <jilmarcode@gmail.com>
2020-09-23 00:49:11 +0000

    Code prints entira alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Print the lowercase alphabet 10 times
+ *
+ * Return : Always return avoid
+ */
+
+void print_alphabet_x10(void)
+{
+	char lowercase;
+	int times = 1;
+
+	for (times = 1; times <= 10; times++)
+	{
+		for (lowercase = 97; lowercase <= 122; lowercase++)
+			_putchar(lowercase);
+	}
+_putchar('\n');
+
+}
