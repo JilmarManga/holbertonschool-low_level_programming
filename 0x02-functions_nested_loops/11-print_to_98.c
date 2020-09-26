11a5d8621fff048b71eb6166f2b302798f5e7f47
JilmarManga <jilmarcode@gmail.com>
2020-09-26 23:28:57 +0000

    Code prints numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - Prints the method
+ *@n: integer to operate
+ *Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	int x;
+
+	if (n <= 98)
+	{
+		for (x = n; x <= 98; x++)
+		{
+			if (x == 98)
+			{
+				printf("%d", x);
+			}
+			else
+			{
+				printf("%d, ", x);
+			}
+		}
+	}
+	else
+	{
+		for (x = n; x >= 98; x--)
+		{
+			if (x == 98)
+                        {
+                                printf("%d", x);
+                        }
+                        else
+                        {
+	                        printf("%d, ", x);
+                        }
+		}
+	}
+	printf("\n");
+}
+x
